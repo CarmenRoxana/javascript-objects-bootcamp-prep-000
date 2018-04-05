cfb67443bf856919afb76778cb40e9d067a280ef
Roxana <roxanafetea@yahoo.com>
2018-04-05 04:02:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,8 @@
 var playlist = {'Tom Jones': 'Lion','Prince': 'Purple Rain', 'David Bowie': 'Blood of Eden'}
 
 function updatePlaylist(playlist, artist, song){
-  
+  playlist.artist = song;
+  return playslist
 }
+
+function 
