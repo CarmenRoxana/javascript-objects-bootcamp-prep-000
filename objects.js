366c13f35771bd5aeffd2b6217f073b4b1d10c86
Roxana <roxanafetea@yahoo.com>
2018-04-05 04:39:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
 var playlist = {'My Bloody Valentine': 'Sometimes'}
 
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
-
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  
-  delete playlist[artistName];
-  return playlist;
-}
+function updatePlaylist(playlist, artistName, songTitle) {
++  playlist[artistName] = songTitle
++  return playlist
++}
++
++function removeFromPlaylist(playlist, artistName) {
++  delete playlist[artistName]
++  return playlist
++}
