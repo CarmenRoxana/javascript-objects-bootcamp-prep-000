e82c46f51a9c28d30d314beeec3b4ffd409913c7
Roxana <roxanafetea@yahoo.com>
2018-04-05 04:21:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
-  return playlist;
+  return Object.assignplaylist.artistName = songTitle;
+
 }
 
 function removeFromPlaylist(playlist, artistName) {
