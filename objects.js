b33a55ac487963e3b224fe98f7896b075d3b132b
Roxana <roxanafetea@yahoo.com>
2018-04-05 04:40:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'My Bloody Valentine': 'Sometimes'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  playlist.artistName= songTitle
   return playlist
 }
 
